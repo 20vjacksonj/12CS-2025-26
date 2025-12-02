34209ea6832c26bc1fdcd64ccf16e7e0ae87fb6e
20vjacksonj <20vjacksonj@hbk.acesmat.uk>
2025-12-02 09:59:53 +0000

    e
    e

--- Exception_handling/main.py
+++ Exception_handling/main.py
@@ -8,3 +8,46 @@ Handling exceptions which are unexpected events in our code such as:
     - file empty 
 '''
 
+
+
+
+'''
+while loop to ask user for number
+'''
+
+exit = False
+while not exit:
+
+    try:
+        num = int(input("Enter a number: "))    #only one thing at a time in a try
+    except ValueError:
+        print("Need a whole number please\n")
+        continue
+    except KeyboardInterrupt:
+        print("\nStop trying to break the system!\n")
+        continue
+    #endexcept
+    exit = True
+
+#endwhile
+
+
+
+'''
+open the file for reading
+'''
+
+myfile = open("H:/github/12CS-2025-26/Exception_handling/results.txt", "r")
+
+exit = False
+while not exit:
+
+    textline = myfile.readline().strip()
+
+    if textline == "":   #EOF reached
+        exit = True
+    else:
+        print(textline)
+    #endif
+
+#endwhile
